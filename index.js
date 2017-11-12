de64659ab3c96ef9a6ae47dd6626a9ae2a5900c7
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-12 18:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-// Code your solution in this file.
-function findMatching(drivers, name) {
-  let matchingDrivers = drivers.filter(function(driver){
-    if (name.toLowerCase === driver.toLowerCase()){
-      return true
-    } else {
-      return false
-    }
+function lowerCaseDrivers(srray){
+  return array.map(function(e){
+    return e.toLowerCase()
   })
-
-  return matchingDrivers;
 }
-
-
-let arr = ["Hello", "World"]
