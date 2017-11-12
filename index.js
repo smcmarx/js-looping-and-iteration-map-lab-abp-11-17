a0e8d6c316c153b41e5cf3bd14624a508e5607fe
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-12 18:06:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solution in this file.
 function findMAtching(drivers, name) {
-  let matchingDrivers = drivers.filter(function(driver)){
+  let matchingDrivers = drivers.filter(function(driver){
     if (name === driver){
       return true
     } else {
       return false
     }
-  }
+  })
 
   return matchingDrivers;
 }
