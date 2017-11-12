4ee1df27889f14343212bc6217426f2287af2515
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-12 18:23:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function lowerCaseDrivers(array){
 }
 
 function nameToAttributes(names){
-  names.map(function(name){
+  return names.map(function(name){
     let fName = name.split(" ")[0]
     let lName = name.split(" ")[1]
     return {firstName: fName, lastName: lName}
