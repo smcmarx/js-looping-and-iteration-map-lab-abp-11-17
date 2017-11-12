20f42190a1bb5b8adbd4b88f9fc46f4823eabec4
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-12 18:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function lowerCaseDrivers(array){
     return e.toLowerCase()
   })
 }
+
+function nameToAttributes(name){
+  names.map(function(name){
+    let fName = name.split(" ")[0]
+    let lName = name.split(" ")[1]
+    return {firstName: fName, lastName: lName}
+  })
+}
